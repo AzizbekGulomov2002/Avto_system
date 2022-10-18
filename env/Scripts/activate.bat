f58a02df7632cd6e7b12f8354c5ff1fe2563aa8a
Azizbek <azizbekgulomov>
2022-10-18 21:03:13 +0500

    'text-fonts-changed'

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\User\Desktop\Avto_systems\Avto_systems\env
+set VIRTUAL_ENV=C:\D disk\Deploy Projects Django\Avto system\env
 
 if not defined PROMPT set PROMPT=$P$G
 
